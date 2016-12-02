4e5bcae100c4a0fd3e4cbf2c22b1d82b866dc866
Thomas Verelst <thomas.verelst@hotmail.com>
2016-12-02 17:20:42 +0100

    Cleaned repo

--- fpga_intro/hw/quartus/unsaved/unsaved_inst.v
+++
@@ -1,7 +0,0 @@
-	unsaved u0 (
-		.clk_clk                            (<connected-to-clk_clk>),                            //                         clk.clk
-		.reset_reset_n                      (<connected-to-reset_reset_n>),                      //                       reset.reset_n
-		.parallelport2_0_conduit_end_export (<connected-to-parallelport2_0_conduit_end_export>), // parallelport2_0_conduit_end.export
-		.parallelport2_1_conduit_end_export (<connected-to-parallelport2_1_conduit_end_export>)  // parallelport2_1_conduit_end.export
-	);
-
--- fpga_seg/hw/modelsim/testbench_config.vhd
+++
@@ -1,7 +0,0 @@
-configuration rtc_tb_conf of rtc_tb is
-   for bench
-      for all : rtc
-         use entity work.rtc;
-      end for; -- all : dprod
-   end for; -- bench
-end configuration rtc_tb_conf;
